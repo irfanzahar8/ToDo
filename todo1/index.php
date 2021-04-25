d729bff532f7c669816d4e4318a5c9728e48d95b
IrfanZahar <afiqirfan08@gmail.com>
2021-04-25 09:21:44 +0800

    add index file

---
+++ todo1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Assignment Kreno</title>
+</head>
+<body>
+	<div class="heading">
+		<h2 style="font-style: 'Hervetica';">ToDo List </h2>
+	</div>
+	<form method="post" action="index.php" class="input_form">
+		<input type="text" name="task" class="task_input">
+		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+	</form>
+</body>
+</html>
